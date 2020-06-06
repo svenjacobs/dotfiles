fc74817837d4128124722383139ee1449d546695
Sven Jacobs <github@svenjacobs.com>
2020-06-06 18:51:32 +0200

    Change bootstrap process

---
+++ README.md
@@ -0,0 +1,4 @@
+# How to set up dotfiles
+
+1) git clone git@github.com:svenjacobs/dotfiles.git $HOME/.dotfiles
+2) $HOME/.dotfiles/setup.sh
---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+cd $HOME
+ln -s .dotfiles/zsh/zprofile .zprofile
+ln -s .dotfiles/zsh/zshenv .zshenv
+ln -s .dotfiles/zsh/zshenv.secret .zshenv.secret
+ln -s .dotfiles/zsh/zshrc .zshrc
+ln -s .dotfiles/zsh/antigenrc .antigenrc
