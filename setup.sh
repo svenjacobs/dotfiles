fbff115220ddcd4f050e3341c31268c0f98ff06b
Sven Jacobs <github@svenjacobs.com>
2020-06-07 15:30:53 +0200

    Add Vim configuration

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env zsh
+
 cd $HOME
+
+# Zsh
 ln -s .dotfiles/zsh/zprofile .zprofile
 ln -s .dotfiles/zsh/zshenv .zshenv
 ln -s .dotfiles/zsh/zshenv.secret .zshenv.secret
 ln -s .dotfiles/zsh/zshrc .zshrc
 ln -s .dotfiles/zsh/antigenrc .antigenrc
+
+# Vim
+ln -s .dotfiles/vim/vimrc .vimrc
